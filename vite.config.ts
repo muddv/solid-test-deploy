dea8993d199324d00fb9ef95eefc2d69680aac29
misha <dedovmu@gmail.com>
2023-04-04 18:43:07 +0300

    set base

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
+  base: '/solid-test-deploy/',
   plugins: [solidPlugin()],
   server: {
     port: 3000,
